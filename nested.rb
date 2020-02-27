d69fda68f9c429f28b203845579d01b5c4bd28df
Luis Pestana <lpestana@protonmail.com>
2020-02-27 16:39:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ programmer_hash[:yukihiro_matsumoto] = {
 	known_for: "Ruby",
 	languages: ["LISP", "C"]
 }
-# programmer_hash[:yukihiro_matsumoto]
+programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
