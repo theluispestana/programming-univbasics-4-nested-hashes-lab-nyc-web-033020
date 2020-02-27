7b42b0a5f73a0fcdcafc8621f22969d081182b2e
Luis Pestana <lpestana@protonmail.com>
2020-02-27 16:44:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,9 +84,6 @@ def adding_matz
 			    :languages => ["LISP", "C"]
 				}
      }
-	# programmer_hash[:yukihiro_matsumoto]
-	# programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-	# programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 	programmer_hash
 end
 
@@ -108,8 +105,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
-
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info	
+		 programmer_hash
 end
 
 def adding_to_dennis
